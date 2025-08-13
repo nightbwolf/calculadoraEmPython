bd1177e6dd263067007472568f7179a4d5aae3a5
nightbwolf <robertovitortroca766@gmail.com>
2025-08-13 20:30:57 -0300

    adicionado variavel de soma

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('hello world');
+a = int(input("escolha um numero: "));
+b = int(input("escolha outro numero: "));
+
+def soma(a,b):
+    resultado = a + b;
+    print(f"a soma dos valores é {resultado}");
+     
+soma(a,b);
+
