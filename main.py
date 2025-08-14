c57afc4ef1c1f71cfffbea4b5c1c3ecc3c85f1d2
nightbwolf <robertovitortroca766@gmail.com>
2025-08-13 21:00:52 -0300

    mend
    adicionando opção de escolha para as operações

--- main.py
+++ main.py
@@ -1,19 +1,3 @@
-escolha = int(input("1 - soma\n2 - subtração\n3 - multiplicação\n4 - divisão\n Escolha a formula: "))
-
-if escolha == 1: 
-    soma(a,b);
-elif escolha == 2: 
-    subt(a,b);
-elif escolha == 3:
-    mult(a,b);
-elif escolha == 4:
-    div(a,b);
-else: 
-    print("escolha inexistente, tente novamente");
-    exit();
-
-a = int(input("escolha um numero: "));
-b = int(input("escolha outro numero: "));
 
 def soma(a,b):
     resultado = a + b;
@@ -38,3 +22,20 @@ def subt(a,b):
     print(f'a subtração dos valores é {resultado}');
 
 #subt(a,b);
+
+a = int(input("escolha um numero: "));
+b = int(input("escolha outro numero: "));
+
+escolha = int(input("1 - soma\n2 - subtração\n3 - multiplicação\n4 - divisão\n Escolha a formula: "));
+if escolha == 1: 
+    soma(a,b);
+elif escolha == 2: 
+    subt(a,b);
+elif escolha == 3:
+    mult(a,b);
+elif escolha == 4:
+    div(a,b);
+else: 
+    print("escolha inexistente, tente novamente");
+    exit();
+
