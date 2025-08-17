110f76b9ffca87e408c9bcd4435f9338f678907d
nightbwolf <robertovitortroca766@gmail.com>
2025-08-17 12:37:57 -0300

    operação de média - para avaliar alunos

--- main.py
+++ main.py
@@ -21,7 +21,9 @@ def mult(a,b):
 def subt(a,b):
     resultado = a-b;
     print(f'a subtração dos valores é {resultado}');
-
+def media(a,b,c,d):
+    resultado = (a+b+c+d)/4
+    print(f'o Resultado da média dos valores é {resultado}');
 #Escolher numeros para operação:
 
 a = int(input("escolha um numero: "));
@@ -29,7 +31,7 @@ b = int(input("escolha outro numero: "));
 
 #Realizar Operação:
 
-escolha = int(input("1 - soma\n2 - subtração\n3 - multiplicação\n4 - divisão\n Escolha a formula: "));
+escolha = int(input("1 - soma\n2 - subtração\n3 - multiplicação\n4 - divisão\n5 - média\n Escolha a formula: "));
 if escolha == 1: 
     soma(a,b);
 elif escolha == 2: 
@@ -38,6 +40,10 @@ elif escolha == 3:
     mult(a,b);
 elif escolha == 4:
     div(a,b);
+elif escolha == 5: #implementa uma função de média colocando mais duas variáveis no código, pode ser usada para avaliar alunos - roberto
+    c = int(input("escolha o terceiro numero:"));
+    d = int(input("escolha o ultimo numero:"));
+    media(a,b,c,d);
 else: 
     print("escolha inexistente, tente novamente");
     exit();
